9314f3a0af22edeebe5be9b66d96c67fc069c01f
Binozo <noah.czaplewski05@gmail.com>
2022-03-15 14:00:33 +0100

    first test

--- index.js
+++ index.js
@@ -11,12 +11,39 @@ try {
 async function start(){
   const telegramBotToken = core.getInput('telegram-bot-token', {required: true, trimWhitespace: true});
   const telegramUserID = core.getInput('telegram-user-id', {required: true, trimWhitespace: true});
-  const telegramChatID = core.getInput("telegram-chat-id", {required: false, trimWhitespace: true});
+  var telegramChatID = core.getInput("telegram-chat-id", {required: false, trimWhitespace: true});
 
   if(telegramChatID.length == 0){
     console.log("No telegram-chat-id provided, searching for Chat with User ID");
-    const telegramChatID = await getTelegramChatID(telegramUserID, telegramBotToken);
+    telegramChatID = await getTelegramChatID(telegramUserID, telegramBotToken);
+
+    if(telegramChatID == null){
+      core.setFailed("No Chat found with User ID");
+      return;
+    }
+
+    console.log("Found Chat with User ID");
   }
+
+  var message = core.getInput('message', {required: false, trimWhitespace: false});
+
+  if(message.length == 0){
+    message = "GitHub Workflow completed";
+  }
+
+  await sendTelegramMessage(telegramChatID, message);
+
+}
+
+async function sendTelegramMessage(chatID, content) {
+  const res = await axios
+    .post(`https://api.telegram.org/bot${telegramBotToken}/sendMessage`, {
+      chat_id: chatID,
+      text: content
+    })
+    .catch(error => {
+      core.setFailed(error)
+    });
 }
 
 async function getTelegramChatID(telegramUserID, telegramBotToken){
@@ -28,11 +55,12 @@ async function getTelegramChatID(telegramUserID, telegramBotToken){
 
     console.log(res.data);
 
-    /*res.result.forEach(element => {
-      console.log(element);
-    });*/
+    res.data.result.forEach(element => {
+      const messageFromUserID = element.message.from.id;
+      if(messageFromUserID == telegramUserID){
+        return element.message.chat.id;
+      }
+    });
 
     return null;
-
-
 }
